6a9ab82e7b663c75d7443c79448d44bbbf2dec99
shrinivas-1998 <shrinivasbodhale9751@gmail.com>
2024-12-22 04:48:58 +0530

    This is second commit by Indian standard

---
+++ src/main/java/shrinivas/Resoruces/shrinivas.java
@@ -0,0 +1,10 @@
+package shrinivas.Resoruces;
+
+public class shrinivas 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("jai shree ram");
+	}
+}
